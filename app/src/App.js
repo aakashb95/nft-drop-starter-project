b2e8b2e265846eb309dfc05f89147d1bbfb8112a
aakashb95 <aakashbakhle@gmail.com>
2021-12-11 12:44:48 +0530

    connect to phantom

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import twitterLogo from './assets/twitter-logo.svg'
 
@@ -7,26 +7,52 @@ const TWITTER_HANDLE = 'aakashb_95'
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState('')
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window
+
       if (solana) {
         if (solana.isPhantom) {
-          console.log('Phantom Wallet found')
-
+          console.log('Phantom wallet found!')
           const response = await solana.connect({ onlyIfTrusted: true })
           console.log(
-            'Connected to Phantom Wallet',
+            'Connected with Public Key:',
             response.publicKey.toString(),
           )
-        } else {
-          alert('Get a phantom wallet to use this app')
+
+          setWalletAddress(response.publicKey.toString())
         }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻')
       }
     } catch (error) {
-      console.log(error)
+      console.error(error)
     }
   }
+
+  /*
+   * Let's define this method so our code doesn't break.
+   * We will write the logic for this next!
+   */
+  const connectWallet = async () => {
+    const { solana } = window
+    if (solana) {
+      const response = await solana.connect()
+      console.log('Connected with Public Key:', response.publicKey.toString())
+      setWalletAddress(response.publicKey.toString())
+    }
+  }
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  )
 
   useEffect(() => {
     const onLoad = async () => {
@@ -42,6 +68,8 @@ const App = () => {
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {/* Render your connect to wallet button right here */}
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -58,3 +86,4 @@ const App = () => {
 }
 
 export default App
+// Nice! You should now have a cool looking gradient button that says "Connect to Wallet" rendered on your page.
